0e25296fd754e5e51a68a0b51ffe8af5a88962a0
Seok Lee <seok.lee@walmart.com>
2017-12-07 21:55:50 -0500

    Tweak boilerplate.py

--- boilerplate.py
+++ boilerplate.py
@@ -1,9 +1,11 @@
-def parse_input(file):
+def parse_input():
     file = open("input.txt", "r'")
     pass
-
 
 def part_one(input): pass
 
 def part_two(input): pass
+
+print part_one(parse_input())
+print part_two(parse_input())
 
